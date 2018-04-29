ffda8a7e6aff698664e4148b5583cc69d5f7fdf1
jdertl <jdertl@uwm.edu>
2018-04-28 21:31:57 -0500

    Adjusted "octree" to now require "octNode" as issues arose in implementing algorithm when it was recursive only Added a "maxDepth" for demponstration Added implementation of algorithm, optimizations could be added Corrected divide by 0 error in "rayProcess" Added test octree for visual only

--- main.cpp
+++ main.cpp
@@ -6,9 +6,15 @@
 
 #define PI 3.1415926536
 
+using namespace std;
+
 struct octNode{
+	bool term;
 	float rgb[3];
 	octNode* nodes[8];
+};
+struct octree{
+	octNode* root;
 	float minX, maxX, minY, maxY, minZ, maxZ;
 };
 
@@ -75,6 +81,8 @@ int angle = 180;
 int height = 100;
 int width = 100;
 
+int maxDepth = 100;
+
 void deleteOctNode(octNode* node){
 	for(int i = 0; i < 8; i++){
 		if(node->nodes[i] != NULL){
@@ -96,6 +104,7 @@ vec3 normalize(const vec3 &v){
 	return v * (1.0 / n);
 }
 
+octree* tree;
 float* displayBuffer = NULL;
 
 void setPixel(int x, int y, float r, float g, float b){
@@ -105,11 +114,147 @@ void setPixel(int x, int y, float r, float g, float b){
 	pixel[2] = b;
 }
 
+bool finalProcess(octNode* node, int x, int y){
+	setPixel(x, y, node->rgb[0], node->rgb[1], node->rgb[2]);
+}
+
+int firstProcess(double tMinX, double tMinY, double tMinZ, double tMaxX, double tMaxY, double tMaxZ){
+	unsigned char index = 0;
+	if(tMinX > tMinY){
+		if(tMinX > tMinZ){
+			if(tMaxY < tMinX){
+				index |= 2;
+			}
+			if(tMaxZ < tMinX){
+				index |= 1;
+			}
+			return index;
+		}
+	}
+	else if(tMinY > tMinZ){
+		if(tMaxX < tMinY){
+			index |= 4;
+		}
+		if(tMaxZ < tMinY){
+			index |= 1;
+		}
+		return index;
+	}
+	if(tMaxX < tMinZ){
+		index |= 4;
+	}
+	if(tMaxY < tMinZ){
+		index |= 2;
+	}
+	return index;
+}
+
+int nextProcess(double tX, int nextX, double tY, int nextY, double tZ, int nextZ){
+	if(tX < tY){
+		if(tX < tZ){
+			return nextX;
+		}
+	}
+	else if(tY < tZ){
+		return nextY;
+	}
+	return nextZ;
+}
+
+bool subProcess(double tMinX, double tMinY, double tMinZ, double tMaxX, double tMaxY, double tMaxZ, int adjust, octNode* node, int depth, int x, int y){
+	if(node && tMaxX >= 0 && tMaxY >= 0 && tMaxZ >= 0){
+		if(depth == maxDepth || node->term){
+			return finalProcess(node, x, y);
+		}
+		double tMidX = (tMinX + tMaxX) * 0.5;
+		double tMidY = (tMinY + tMaxY) * 0.5;
+		double tMidZ = (tMinZ + tMaxZ) * 0.5;
+		int curIndex = firstProcess(tMinX, tMinY, tMinZ, tMidX, tMidY, tMidZ);
+		bool pass = false;
+		while(curIndex < 8){
+			switch(curIndex){
+				case 0:
+					pass = subProcess(tMinX, tMinY, tMinZ, tMidX, tMidY, tMidZ, adjust, node->nodes[adjust], depth + 1, x, y);
+					curIndex = nextProcess(tMidX, 4, tMidY, 2, tMidZ, 1);
+					break;
+				case 1:
+					pass = subProcess(tMinX, tMinY, tMidZ, tMidX, tMidY, tMaxZ, adjust, node->nodes[1^adjust], depth + 1, x, y);
+					curIndex = nextProcess(tMidX, 5, tMidY, 3, tMaxZ, 8);
+					break;
+				case 2:
+					pass = subProcess(tMinX, tMidY, tMinZ, tMidX, tMaxY, tMidZ, adjust, node->nodes[2^adjust], depth + 1, x, y);
+					curIndex = nextProcess(tMidX, 6, tMaxY, 8, tMidZ, 3);
+					break;
+				case 3:
+					pass = subProcess(tMinX, tMidY, tMidZ, tMidX, tMaxY, tMaxZ, adjust, node->nodes[3^adjust], depth + 1, x, y);
+					curIndex = nextProcess(tMidX, 7, tMaxY, 8, tMaxZ, 8);
+					break;
+				case 4:
+					pass = subProcess(tMidX, tMinY, tMinZ, tMaxX, tMidY, tMidZ, adjust, node->nodes[4^adjust], depth + 1, x, y);
+					curIndex = nextProcess(tMaxX, 8, tMidY, 6, tMidZ, 5);
+					break;
+				case 5:
+					pass = subProcess(tMidX, tMinY, tMidZ, tMaxX, tMidY, tMaxZ, adjust, node->nodes[5^adjust], depth + 1, x, y);
+					curIndex = nextProcess(tMaxX, 8, tMidY, 7, tMaxZ, 8);
+					break;
+				case 6:
+					pass = subProcess(tMidX, tMidY, tMinZ, tMaxX, tMaxY, tMidZ, adjust, node->nodes[6^adjust], depth + 1, x, y);
+					curIndex = nextProcess(tMaxX, 8, tMaxY, 8, tMidZ, 7);
+					break;
+				case 7:
+					pass = subProcess(tMidX, tMidY, tMidZ, tMaxX, tMaxY, tMaxZ, adjust, node->nodes[7^adjust], depth + 1, x, y);
+					curIndex = 8;
+					break;
+			}
+			if(pass){
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+void rayProcess(vec3 orig, vec3 dir, int x, int y){
+	unsigned char adjust = 0;
+	if(dir.x < 0){
+		orig.x = ((tree->minX + ((tree->maxX - tree->minX) / 2.0)) * 2.0) - orig.x;
+		dir.x = -dir.x;
+		adjust |= 4;
+	}
+	if(dir.y < 0){
+		orig.y = ((tree->minY + ((tree->maxY - tree->minY) / 2.0)) * 2.0) - orig.y;
+		dir.y = -dir.y;
+		adjust |= 2;
+	}
+	if(dir.z < 0){
+		orig.z = ((tree->minZ + ((tree->maxZ - tree->minZ) / 2.0)) * 2.0) - orig.z;
+		dir.z = -dir.z;
+		adjust |= 1;
+	}
+	if(dir.x == 0){
+		dir.x = FLT_MIN;
+	}
+	if(dir.y == 0){
+		dir.y = FLT_MIN;
+	}
+	if(dir.z == 0){
+		dir.z = FLT_MIN;
+	}
+	double tMinX = (tree->minX - orig.x) / dir.x;
+	double tMinY = (tree->minY - orig.y) / dir.y;
+	double tMinZ = (tree->minZ - orig.z) / dir.z;
+	double tMaxX = (tree->maxX - orig.x) / dir.x;
+	double tMaxY = (tree->maxY - orig.y) / dir.y;
+	double tMaxZ = (tree->maxZ - orig.z) / dir.z;
+	if(max(max(tMinX, tMinY), tMinZ) < min(min(tMaxX, tMaxY), tMaxZ)){
+		subProcess(tMinX, tMinY, tMinZ, tMaxX, tMaxY, tMaxZ, adjust, tree->root, 1, x, y);
+	}
+}
+
 void displayFunc(){
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	vec3 origin, dir;
-	float* pixel;
 	for(int y = 0; y < height; y++){
 		for(int x = 0; x < width; x++){
 			origin = viewpoint + (vp_u * ((1.0 / width) * (x + 0.5) - 0.5)) + (vp_v * ((1.0 / height) * (y + 0.5) - 0.5)) + (vp_w * vp_d);
@@ -125,6 +270,8 @@ void displayFunc(){
 					setPixel(x, y, 1.0, 0.0, 0.0);
 				}
 			}
+
+			rayProcess(origin, dir, x, y);
 		}
 	}
 
@@ -208,8 +355,41 @@ void reshapeFunc(int newWidth, int newHeight){
 	glClear(GL_COLOR_BUFFER_BIT);
 }
 
+void specialFunc(int key, int x, int y){
+	switch(key){
+		case GLUT_KEY_UP:
+			maxDepth++;
+			break;
+		case GLUT_KEY_DOWN:
+			if(maxDepth > 1){
+				maxDepth--;
+			}
+			break;
+	}
+	glutPostRedisplay();
+}
+
 int main(int argc, char** argv){
-	//octNode temp = octNode();
+	tree = new octree();
+	tree->maxX = 2;
+	tree->minX = 0.5;
+	tree->maxY = 2;
+	tree->minY = 0.5;
+	tree->maxZ = 2;
+	tree->minZ = 0.5;
+	tree->root = new octNode();
+
+	octNode* node = tree->root;
+	for(int i = 2; i < 7; i++){
+		node->nodes[i] = new octNode();
+		node->nodes[i]->term = true;
+		node->nodes[i]->rgb[0] = rand() / (float) RAND_MAX;
+		node->nodes[i]->rgb[1] = rand() / (float) RAND_MAX;
+		node->nodes[i]->rgb[2] = rand() / (float) RAND_MAX;
+	}
+
+
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
@@ -224,6 +404,7 @@ int main(int argc, char** argv){
 	glutDisplayFunc(displayFunc);
 	glutKeyboardFunc(keyboardFunc);
 	glutReshapeFunc(reshapeFunc);
+	glutSpecialFunc(specialFunc);
 
 	glutMainLoop();
 }
